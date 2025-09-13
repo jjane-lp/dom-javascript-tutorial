464a27ce3a44acf63edb2b373685b6a34035dd5f
jjane-lp <jjanembabazi@gmail.com>
2025-09-13 11:42:00 +0300

    add conditionals

--- main.js
+++ main.js
@@ -18,3 +18,34 @@ console.log("procedural programs")
 
 const Name = "Henry"; console.log(Name)
 const name = "Henry"; console.log(name)
+
+
+//legal  variable names
+
+var Number_hits = ''
+var temp99 = ''
+var $credit = ''
+var _name = ''
+
+var x=45
+console.log(x)
+let x=50;
+console.log(x)
+
+// conditionals
+// variable scope
+late = false // or true
+gi
+if(late){
+    console.log("Finish class late")
+} else if(!late){
+     console.log("Finish class early")
+}else {
+    console.log("Class completed")
+}
+
+if(late){
+    console.log("Finish class late")
+}else{
+    console.log("finish class early")
+}
