4c4d22840111489e07ee013e41c26cf1a0ebe87b
jjane-lp <jjanembabazi@gmail.com>
2025-09-13 11:12:45 +0300

    and comments and print statements

---
+++ main.js
@@ -0,0 +1,20 @@
+// console.log("THIS IS A PRINT ") this is a one line comment
+
+/*
+this is a longer 
+
+comment
+
+*/
+
+
+console.log("THE PARADIGNS OF PROGRAMING LANGUAGES") // this is a print
+
+console.log("functional") //this is a comment
+
+console.log("object oriented program")
+
+console.log("procedural programs")
+
+const Name = "Henry"; console.log(Name)
+const name = "Henry"; console.log(name)
